7042958d92e463d1bb986b0a6c7dfe54381049d9
Elmer Cruz <elmer06.cruz@gmail.com>
2025-02-01 19:18:56 -0600

    Subiendo todo el proyecto a github

---
+++ src/components/Form.jsx
@@ -0,0 +1,47 @@
+import React, {useState} from "react";
+import Todo from "./Todo";
+
+const Form = () => {
+    const [todo, setTodo] = useState({})
+    const [todos, setTodos] = useState([
+        { todo: 'Todo 1' },
+        { todo: 'Todo 2' },
+        { todo: 'Todo 3' }
+    ])
+
+    const handleChange = e => setTodo({[e.target.name]: e.target.value })
+    const handleClick = e => {
+        if (Object.keys(todo).length === 0 || todo.todo.trim() === '') {
+            alert('El campo no puede estar vacio')
+            return
+        }
+        setTodos([...todos, todo])
+    }
+
+    const deleteTodo = indice => {
+        const newTodos = [...todos]
+        newTodos.splice(indice, 1)
+        setTodos(newTodos)
+    }
+
+    return (
+        <>
+            <form onSubmit={e => e.preventDefault()}>
+                <div className="input-container">
+                    <input type="text" name="todo" placeholder="Agregar tarea" onChange={handleChange} />
+                    <button title="Agregar" className="agregar" onClick={handleClick}>+</button>
+                </div>
+            </form>
+            <h3>Lista</h3>
+            {
+                todos.map((value, index) => (
+                    <Todo todo={value.todo}
+                    
+                     key={index} index={index} deleteTodo={deleteTodo} />
+                ))
+            }
+        </>
+    )
+}
+
+export default Form;
